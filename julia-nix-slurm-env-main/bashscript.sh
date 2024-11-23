185eadcc0daba5b0b9e307484a0ecef4b9b00b85
ebertzci <cindy.ebertz@student.uni-augsburg.de>
2024-11-23 19:34:15 +0100

    added new server stuff

---
+++ julia-nix-slurm-env-main/bashscript.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sbatch /data/oc-compute03/cuihenni/testing2/scripts/dothestuff.sbatch
+sleep .5
+sbatch /data/oc-compute03/cuihenni/testing2/scripts/dothestuff2.sbatch
---
+++ julia-nix-slurm-env-main/scripts/exampleScript.jl
@@ -0,0 +1,8 @@
+
+println("Hello World")
+
+println(ARGS, "\n")
+println("Args 1: ", ARGS[1])
+println("Args 2: ", ARGS[2])
+
+println("End")
