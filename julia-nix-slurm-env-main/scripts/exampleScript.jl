25e1534e9848067b25a296efe3d92220432b13bd
ebertzci <cindy.ebertz@student.uni-augsburg.de>
2024-11-23 20:07:42 +0100

    still server stuff

--- julia-nix-slurm-env-main/scripts/exampleScript.jl
+++
@@ -1,8 +0,0 @@
-
-println("Hello World")
-
-println(ARGS, "\n")
-println("Args 1: ", ARGS[1])
-println("Args 2: ", ARGS[2])
-
-println("End")
--- julia-nix-slurm-env-main/scripts/exampleScript2.jl
+++
@@ -1,9 +0,0 @@
-
-println("Hello World 2")
-
-println(ARGS, "\n")
-println("Args 1: ", ARGS[1])
-println("Args 2: ", ARGS[2])
-println("Args 3: ", ARGS[3])
-
-println("End2")
